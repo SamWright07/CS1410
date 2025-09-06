950ee59263d979aee7040ead2150289c9304d101
HomelessMan27 <wrightsa07@gmail.com>
2025-09-06 02:52:11 +0000

    new file:   .devcontainer/Dockerfile 	new file:   .devcontainer/devcontainer.json 	new file:   .devcontainer/reinstall-cmake.sh 	new file:   .vscode/tasks.json 	new file:   src/main 	new file:   src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // Assignment 1
+    std::cout << "Name: Sam Wright" << std::endl;
+    std::cout << "GitHub Username: SamWright07" << std::endl;
+
+    return 0;
+}
