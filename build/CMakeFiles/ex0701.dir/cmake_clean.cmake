718df00719200a123e4692047dd9fa78b3cb6de2
SamWright07 <wrightsa07@gmail.com>
2025-10-23 03:05:21 +0000

    Video 7

---
+++ build/CMakeFiles/ex0701.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex0701.dir/src/ex0701.cpp.o"
+  "ex0701"
+  "ex0701.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex0701.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
